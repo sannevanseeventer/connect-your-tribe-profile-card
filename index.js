89f1bd8f4dd86f2eed3ba96648a5d9e907fa4682
sannevanseeventer <sanne.van.seeventer@hva.nl>
2023-02-13 08:50:01 +0100

    redesign visitekarartje

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // Importeer express uit de node_modules map
-import express from 'express'
+import express, { response } from 'express'
+
+const url = 'https://whois.fdnd.nl/api/v1/member/sanne-van-seeventer'
+const data = await fetch(url).then((response) => response.json())
+
+// console.log(data)
 
 // Maak een nieuwe express app aan
 const app = express()
@@ -12,9 +17,9 @@ app.set('views', './views')
 app.use(express.static('public'))
 
 // Maak een route voor de index
-app.get('/', function (req, res) {
+app.get('/', function (req, res) { // app.get = 'wanneer we een bezoeker krijgen'
   // res.send('Hello World!')
-  res.render('index')
+  res.render('index', data) // res.render = 'genereer een pagina om te laten zien'
 })
 
 // Stel het poortnummer in waar express op gaat luisteren
@@ -25,3 +30,4 @@ app.listen(app.get('port'), function () {
   // Toon een bericht in de console en geef het poortnummer door
   console.log(`Application started on http://localhost:${app.get('port')}`)
 })
+
---
+++ public/js/script.js
@@ -0,0 +1,48 @@
+const flipCardContainer = document.querySelector(".flip-card-container");
+const no = document.querySelector(".no");
+const info = document.querySelector(".info");
+const yes = document.querySelector(".yes");
+const return1 = document.querySelector(".return1");
+const no2= document.querySelector(".no2");
+
+
+no.addEventListener("click", function() {
+    flipCardContainer.classList.add ("option-3");
+    flipCardContainer.classList.add ("flip");
+    console.log('NOPEE')
+});
+
+info.addEventListener("click", function() {
+    flipCardContainer.classList.add ("option-2");
+    flipCardContainer.classList.add ("flip");
+
+    console.log('INFOOOOO')
+});
+
+yes.addEventListener("click", function() {
+    flipCardContainer.classList.add ("option-1");
+    flipCardContainer.classList.add ("flip");
+
+    console.log('YESSSS')
+});
+
+return1.addEventListener("click", function() {
+    flipCardContainer.classList.add ("option-4");
+    flipCardContainer.classList.add ("flip");
+
+    console.log('BACKKK')
+});
+
+no2.addEventListener("click", function() {
+    flipCardContainer.classList.add ("option-4");
+    flipCardContainer.classList.add ("flip");
+
+    console.log('FRONTTT')
+});
+
+$('a').mouseover(function(){
+    $('span:first-child').hide();
+  });
+  $('a').mouseleave(function(){
+    $('span:first-child').fadeIn(400);
+  });
